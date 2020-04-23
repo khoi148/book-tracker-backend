bc8c422f0c1749b155d1a8883292fe35ee518372
khoi148 <khoiminhphan@yahoo.com>
2020-04-23 18:16:03 +0700

    added await

--- controller/loginCont.js
+++ controller/loginCont.js
@@ -7,7 +7,7 @@ exports.loginUser = async function (req, res) {
     if (!email && !password) throw new Error("Email and password are required");
     // authenticate user
     const user = await User.loginWithEmail(email, password);
-    const token = user.generateToken(); //saves token to user, and returns the token
+    const token = await user.generateToken(); //saves token to user, and returns the token
     res.json({ status: "success", data: { user, token } });
   } catch (err) {
     res.status(400).json({ status: "fail", message: err.message });
